ddf8c359654359729822a2df1aeb523acd379a4b
Bon <bonhiki1233@gmail.com>
2026-04-17 00:55:43 +0700

    member 3: hoàn thành tính calories + vẽ lộ trình chạy ở màn hình Summary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.osmdroid:osmdroid-android:6.1.18")
 }
