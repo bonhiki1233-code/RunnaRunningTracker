f30ded10a48e50cfbd59660bd46df023cfcc21df
khoadonguyen1312 <khoadonguyen8@gmail.com>
2026-04-16 14:38:26 +0700

    feature lich su va thong ke tuan

--- app/src/main/java/com/example/runna_runningtracker/SummaryActivity.kt
+++ app/src/main/java/com/example/runna_runningtracker/SummaryActivity.kt
@@ -2,12 +2,27 @@ package com.example.runna_runningtracker
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.runna_runningtracker.data.model.Run
+import com.example.runna_runningtracker.data.repository.AuthRepository
+import com.example.runna_runningtracker.data.repository.RunsHistoryRepository
 
 class SummaryActivity : AppCompatActivity() {
+    private  var runId: String ="";
+    private var distance: Double =0.0;
 
+    var calories :Int =0;
+    var startTime: Long =0L;
+    var endTime: Long =0L
+    var duration: Int =0;
+
+    var pace : Double =0.0;
+
+    var run_type :String ="";
+    private val TAG:String ="SummaryActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
@@ -17,10 +32,15 @@ class SummaryActivity : AppCompatActivity() {
         val tvPace = findViewById<TextView>(R.id.tvSumPace)
         val btnDone = findViewById<Button>(R.id.btnDone)
 
-        val duration = intent.getIntExtra("duration",0)
-        val distance = intent.getDoubleExtra("distance",0.0)
-        val pace = intent.getDoubleExtra("pace",0.0)
+         duration = intent.getIntExtra("duration",0)
+         distance = intent.getDoubleExtra("distance",0.0)
+         pace = intent.getDoubleExtra("pace",0.0)
+        runId = intent.getStringExtra("run_id") ?: ""
 
+         calories = intent.getIntExtra("calories", 0)
+         startTime = intent.getLongExtra("start_time", 0L)
+         endTime = intent.getLongExtra("end_time", 0L)
+        run_type =intent.getStringExtra("RUN_MODE")?:""
         val minutes = duration / 60
         val seconds = duration % 60
 
@@ -33,10 +53,36 @@ class SummaryActivity : AppCompatActivity() {
         tvPace.text = String.format("%d:%02d", paceMin, paceSec)
 
         btnDone.setOnClickListener {
+
+            // goi ham save lich su
+            saveRun();
             val intent = Intent(this, HomeActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
             finish()
+
+
         }
+
+    }
+    fun saveRun()
+    {
+        val userid = AuthRepository().getCurrentUserId();
+        if(userid!=null)
+        {
+            Log.d(TAG,"lay thanh cong userid")
+            val runData = Run(runId,userid,distance, duration,pace,calories,startTime,endTime,run_type)
+            RunsHistoryRepository.save(runData,){
+                    bool ->
+                if(bool)
+                {
+                    Log.d(TAG,"luu thanh cong lich su")
+                }
+                else{
+                    Log.e(TAG,"khong luu duoc lich su chay")
+                }
+            }
+        }
+        Log.e(TAG,"co loi khi lay userid khong luu duoc lich su run")
     }
 }
