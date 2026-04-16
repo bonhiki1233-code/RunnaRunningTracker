e043c76c8407bec251ec965ee1f6f7e719056b8f
Lyche <ngonghi117@gmail.com>
2026-04-16 21:04:47 +0700

    Revert "Merge pull request #12 from bonhiki1233-code/master"
    This reverts commit 1f2a5a7e26ccd2aa08ac9767a4bafdab2ab05555, reversing
    changes made to 10ec5ba8151093aa07552b733343b76bb6135e7a.

--- app/src/main/java/com/example/runna_runningtracker/RunningActivity.kt
+++ app/src/main/java/com/example/runna_runningtracker/RunningActivity.kt
@@ -18,8 +18,6 @@ import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Polyline
 import java.util.Locale
 import org.osmdroid.views.overlay.Marker
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import java.util.UUID
 
 class RunningActivity : AppCompatActivity() {
@@ -89,9 +87,7 @@ class RunningActivity : AppCompatActivity() {
 
         btnPause.setOnClickListener {
             running = false
-            val timeString = tvDurationClock.text.toString()
-            findViewById<TextView>(R.id.tvPauseTime).text = timeString
-            //findViewById<TextView>(R.id.tvPauseTime).text = tvDurationClock.text
+            findViewById<TextView>(R.id.tvPauseTime).text = tvDurationClock.text
             pausePanel.visibility = View.VISIBLE
         }
 
@@ -101,33 +97,38 @@ class RunningActivity : AppCompatActivity() {
         }
 
         btnOverlayFinish.setOnClickListener {
-            running = false
+
             val distanceKm = totalDistance / 1000.0
+            val pace = if (distanceKm > 0)
+                (seconds / 60.0) / distanceKm
+            else 0.0
 
-        val userWeight = UserPrefsManager.getUserWeightOrNull(this)
-        val finalCalories = if (userWeight != null && userWeight > 0) {
-            (1.036 * userWeight * distanceKm).toInt()
-        } else {
-            (distanceKm * 60).toInt()
+            val intent = Intent(this, SummaryActivity::class.java)
+            intent.putExtra("distance", distanceKm)
+            intent.putExtra("duration", seconds)
+            intent.putExtra("pace", pace)
+            intent.putExtra("run_id",run_id)
+            intent.putExtra("end_time", System.currentTimeMillis())
+            intent.putExtra("start_time",startTime)
+            intent.putExtra("calories",calories)
+            intent.putExtra("RUN_MODE",run_type);
+//            data class Run(
+//                val run_id: String = "",
+//                val user_id: String = "",
+//                val distance: Double = 0.0,
+//                val durationSeconds: Int = 0,
+//                val pace: Double = 0.0,
+//                val calories: Int = 0,
+//                val start_time: Long = 0L,
+//                val end_time : Long =0L,
+
+//            )
+
+            startActivity(intent)
+            finish()
         }
 
-        val pathPoints = polyline.actualPoints
-        val pathJson = Gson().toJson(pathPoints)
-
-        val intent = Intent(this, SummaryActivity::class.java).apply {
-            putExtra("distance", distanceKm)
-            putExtra("duration", seconds)
-            putExtra("pace", if (distanceKm > 0) (seconds / 60.0) / distanceKm else 0.0)
-            putExtra("calories", finalCalories)
-            putExtra("path_data", pathJson)
-            putExtra("run_id", run_id)
-            putExtra("start_time", startTime)
-            putExtra("end_time", System.currentTimeMillis())
-            putExtra("RUN_MODE", run_type)
-        }
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-        startActivity(intent)
-        finish()
+        startLocationUpdates()
     }
 
     private fun startLocationUpdates() {
@@ -186,12 +187,8 @@ class RunningActivity : AppCompatActivity() {
 
                 val distanceKm = totalDistance / 1000.0
                 tvDistanceMain.text = String.format("%.2f", distanceKm)
-                val userWeight = UserPrefsManager.getUserWeightOrNull(this@RunningActivity)
-                val calories = if (userWeight != null && userWeight > 0) {
-                    (1.036 * userWeight *distanceKm).toInt()
-                } else {
-                    (distanceKm * 60).toInt()
-                }
+
+                 calories = (distanceKm * 60).toInt()
                 tvCaloriesMain.text = calories.toString()
 
                 if (distanceKm > 0.05) {
@@ -244,7 +241,6 @@ class RunningActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacksAndMessages(null)
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
     override fun onRequestPermissionsResult(
